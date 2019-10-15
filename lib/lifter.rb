3afe3cecab89a1f2c8ea59243981df01e9c8241c
Kuran <kurankumar@gmail.com>
2019-10-15 01:29:20 -0400

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,37 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.lifter == self
+    end
+  end
+
+  def gyms
+    memberships.map do |membership|
+      membership.gym
+    end
+  end
+
+  def self.average_lift
+    total_lift = @@all.inject(0) do |sum, lifter|
+      sum += lifter.lift_total
+    end
+    average_lift = total_lift / @@all.length
+  end
+
+  def create_membership(gym, cost)
+    Membership.new(cost, self, gym)
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,12 @@
 # You don't need to require any of the files in lib or pry.
 # We've done it for you here.
-require_relative '../config/environment.rb'
+require_relative "../config/environment.rb"
 
 # test code goes here
+
+g1 = Gym.new("golds")
+l1 = Lifter.new("kuran", 50)
+m1 = Membership.new(150, l1, g1)
 
 binding.pry
 
